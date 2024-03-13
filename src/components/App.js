9831d253041472aedb46141a9c0e7500360e6655
oliverfont <oliverfont@protonmail.com>
2024-03-13 12:38:12 +0100

    Mise en page header et banner

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import logo from '../logo.svg';
+import '../App.css';
+import Header from './Header'
+import Banner from './Banner'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Banner />
+    </div>
+  );
+}
+
+export default App;
