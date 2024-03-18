8a3242552c5d6c8cf581beceb7f2601b18294362
oliverfont <oliverfont@protonmail.com>
2024-03-18 11:51:28 +0100

    première mise en page1 sans route

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
-import logo from '../logo.svg';
 import '../App.css';
 import Header from './Header'
 import Banner from './Banner'
+import Articles from './Articles'
+import Footer from './Footer'
 
 function App() {
   return (
-    <div>
+    <div className='body'>
       <Header />
       <Banner />
+      <Articles />
+      <Footer />
     </div>
   );
 }
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,7 +2,7 @@ import background from '../assets/IMG.png'
 import '../styles/Banner.css'
 
 function Banner() {
-    return <div class="banner">
+    return <div className="banner">
                 <img src={background} alt='background banner' className='bgBanner' />
                 <h1>Chez vous, partout et ailleurs</h1>
             </div>
